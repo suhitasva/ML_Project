3486900bf2b48a02849d7886899fb5af1e248bf9
suhitasva <suhitasucheta@hotmail.com>
2022-03-16 11:22:56 -0400

    Created functions for test NA values and to change dtype for some numerical columns

--- clean_helper.py
+++ clean_helper.py
@@ -37,3 +37,38 @@ def common_na_replace(df):
     # Replacing NA year values in GarageYrBlt to house YearBuilt:
 
     df['GarageYrBlt'].fillna(df['YearBuilt'], inplace=True)
+
+def test_na_replace(df):
+
+    # Replacing NA values in categorical variables:
+    
+    df['KitchenQual'].replace(np.nan,'TA',inplace = True)
+    df['Functional'].replace(np.nan,'Typ',inplace = True)
+    df['SaleType'].replace(np.nan,'Oth',inplace = True)
+  
+    # Replacing NA value in MSZoning, Utilities columns with the most 
+    # common values
+
+    df['MSZoning'].replace(np.nan,df['MSZoning'].mode()[0],inplace = True)
+    df['Utilities'].replace(np.nan,df['Utilities'].mode()[0],inplace = True)
+
+    for col in ('Exterior1st', 'Exterior2nd'):\
+    df[col] = df[col].fillna('Other')
+
+    for col in ('BsmtFinSF1', 'BsmtFinSF2',\
+                'BsmtUnfSF', 'TotalBsmtSF',
+                'BsmtFullBath','BsmtHalfBath',
+                'GarageCars','GarageArea'):\
+    df[col] = df[col].fillna(0)
+
+def dtype_update(df):
+
+    df['MSSubClass'] = df['MSSubClass'].astype(str)
+    df['OverallQual'] = df['OverallQual'].astype(str)
+    df['OverallCond'] = df['OverallCond'].astype(str)
+    df['YearBuilt'] = df['YearBuilt'].astype(str)
+    df['YearRemodAdd'] = df['YearRemodAdd'].astype(str)
+    df['GarageYrBlt'] = df['GarageYrBlt'].astype(int)
+    df['GarageYrBlt'] = df['GarageYrBlt'].astype(str)
+    df['MoSold'] = df['MoSold'].astype(str)
+    df['YrSold'] = df['YrSold'].astype(str)
